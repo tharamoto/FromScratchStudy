01115a56c32150816724077cacd36c7182727a30
tharamoto <tharamoto@obc.co.jp>
2023-02-24 17:11:37 +0900

    first commit

---
+++ dataset/minist.py
@@ -0,0 +1,62 @@
+﻿try:
+    import urllib.request
+except ImportError:
+    raise ImportError("You should use Pythn 3.x")
+
+import os.path
+import gzip
+import pickle
+import numpy as np
+
+url_base = "http://yann.lecun.om/exdb/mnist^/"
+key_file = {
+    'train_img':'train-images-idx3-ubyte.gz',
+    'train_label':'train-labels-idx1-ubyte.gz',
+    'test_img':'t10k-images-idx3-ubyte.gz',
+    'test_label':'t10k-labels-idx1-ubyte.gz'
+}
+
+dataset_dir = os.path.dirname(os.path.abspath(__file__))
+save_file = dataset_dir + "/mnist.pkl"
+
+train_num = 60000
+test_num = 10000
+img_dim  = (1, 28, 28)
+img_size = 784
+
+def _load_img(file_name):
+    file_path = dataset_dir + "/" + file_name
+
+    print("Converting " + file_name + " to Numpy Array ...")
+    with gzip.open(file_path, "rb") as f:
+        data = np.frombuffer(f.read(), np.uint8, offset=16)
+    data = data.reshape(-1, img_size)
+    print("Done")
+
+    return data
+    
+def _convert_numpy():
+    dataset = {}
+    dataset['train_img'] = _load_img(key_file['train_img'])
+    dataset['train_label'] = _load_img(key_file['train_label'])
+    dataset['test_img'] = _load_img(key_file['test_img'])
+    dataset['test_label'] = _load_img(key_file['test_label'])
+
+    return dataset
+
+
+def init_mnist():
+    download_mnist()
+    dataset = _convert_numpy()
+    print("Creating pickle file ...")
+    with open(save_file, 'wb') as f:
+        pickle.dump(dataset, f, -1)
+
+    print("Done!")
+
+
+def download_mnist():
+    print("")
+
+if __name__ == '__main__':
+    init_mnist()
