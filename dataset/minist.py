283b88c3a76065bc04ea70633c99de3357e4ba25
tharamoto <tharamoto@obc.co.jp>
2023-03-06 17:20:38 +0900

    2023/03/06 -- Study

--- dataset/minist.py
+++ dataset/minist.py
@@ -75,7 +75,49 @@ def init_mnist():
 
     print("Done!")
 
+def _change_one_hot_label(X):
+    T = np.zeros((X.size, 10))
+    for idx, row in enumerate(T):
+        row[X[idx]] = 1
+        
+    return T
 
+def load_mnist(normalize=True, flatten=True, one_hot_label=False):
+    """MNISTデータセットの読込
+
+    Parameters
+    ----------
+    normalize : 画像のピクセル地を0.0~1.0に正規化する
+    one_hot_label :
+        one_hot_labelがTrueの場合、ラベルはone-hot配列として返す
+        one_hot配列とは、たとえば[0,0,1,0,0,0,0,0,0,0]のような配列
+    flatten : 画像を一次元配列に平にするかどうか
+    
+    Returns
+    -------
+    (訓練画像, 訓練ラベル), (テスト画像, テストラベル)
+    """
+    
+    if not os.path.exists(save_file):
+        init_mnist()
+        
+    with open(save_file, 'rb') as f:
+        dataset = pickle.load(f)
+        
+    if normalize:
+        for key in ('train_img', 'test_img'):
+            dataset[key] = dataset[key].astype(np.float32)
+            dataset[key] /= 255.0
+            
+    if one_hot_label:
+        dataset['train_label'] = _change_one_hot_label(dataset['train_label'])
+        dataset['test_label'] = _change_one_hot_label(dataset['test_label'])
+        
+    if not flatten:
+        for key in ('train_img', 'test_img'):
+            dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
+
+    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label'])
 
 if __name__ == '__main__':
     init_mnist()
