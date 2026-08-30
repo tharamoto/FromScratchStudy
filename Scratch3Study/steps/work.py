d2972c55e516f8d093224303969bfc5959d6cd00
tharamoto <tharamoto0101@gmail.com>
2026-08-30 17:00:59 +0900

    20260830 LifeWork Cafe 西荻窪

---
+++ Scratch3Study/steps/work.py
@@ -0,0 +1,22 @@
+﻿# import numpy as np
+# from dezero.core_simple import Variable
+
+# x = Variable(np.array(1.0))
+# print(x)
+
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+
+import numpy as np
+from dezero import Variable
+
+x = Variable(np.array(1.0))
+y = (x + 3) ** 2
+y.backward()
+
+print(y)
+print(x.grad)
+
+
