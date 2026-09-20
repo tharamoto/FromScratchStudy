f171941cee15f9c1dd7761d351370596cf6f39fa
tharamoto <tharamoto0101@gmail.com>
2026-09-20 17:13:37 +0900

    20260920 SHARELOUNGE MARUNOUCHI

---
+++ Scratch3Study/dezero/utils.py
@@ -0,0 +1,17 @@
+﻿import os
+import subprocess
+import numpy as np
+
+
+def _dot_var(v, verbose=False):
+    dot_var = '{} [label="{}", color=orage, style=filled]\n'
+
+    name = '' if v.name is None else v.name
+    if verbose and v.data is not None:
+        name += ': '
+
+    name += str(v.shape) + ' ' + str(v.dtype)
+
+    return dot_var.format(id(v), name)
+
+
--- Scratch3Study/steps/work.py
+++ Scratch3Study/steps/work.py
@@ -4,11 +4,12 @@
 # x = Variable(np.array(1.0))
 # print(x)
 
+
 if '__file__' in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-
+"""
 import numpy as np
 from dezero import Variable
 
@@ -18,5 +19,14 @@ y.backward()
 
 print(y)
 print(x.grad)
+ """
+
+import numpy as np
+from dezero import Variable
+from dezero.utils import _dot_var
 
 
+x = Variable(np.random.randn(2,3))
+x.name = 'x'
+print(_dot_var(x))
+print(_dot_var(x, verbose=True))
