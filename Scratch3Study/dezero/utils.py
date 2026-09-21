d96deee7d5e2f448d7b83cac40c9ca112aa457f7
tharamoto <tharamoto0101@gmail.com>
2026-09-21 17:39:48 +0900

    20260921 自宅(めずらしい)

--- Scratch3Study/dezero/utils.py
+++ Scratch3Study/dezero/utils.py
@@ -15,3 +15,40 @@ def _dot_var(v, verbose=False):
     return dot_var.format(id(v), name)
 
 
+def _dot_func(f):
+    dot_func = '{} [label="{}", color=lightblue, style=filled, shape=box]\n'
+    txt = dot_func.format(id(f), f.__class__.__name__)
+
+    dot_edge = '{} -> {}\n'
+    for x in f.inputs:
+        txt += dot_edge.format(id(x), id(f))
+    for y in f.outputs:
+        txt += dot_edge.format(id(f), id(y())) # y is weakref
+
+    return txt
+
+def get_dot_graph(output, verbose=True):
+    txt = ''
+    funcs = []
+    seen_set = set()
+
+    def add_func(f):
+        if f not in seen_set:
+            funcs.append(f)
+            # funcs.sort(key=lambda x: x.generation)
+            seen_set.add(f)
+
+    add_func(output.creator)
+    txt += _dot_var(output, verbose)
+
+    while funcs:
+        func = funcs.pop()
+        txt += _dot_func(func)
+        for x in func.inputs:
+            txt += _dot_var(x, verbose)
+
+            if x.creator is not None:
+                add_func(x.creator)
+
+        return 'digraph g {\n' + txt + '}'
+    
